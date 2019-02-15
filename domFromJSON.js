5ee2587de418e440f1cdd2e987a8b9bfd689f308
kunalbohra <kunal.bohraji.gmail.com>
2019-02-14 22:47:28 -0800

    Adding comments

--- domFromJSON.js
+++ domFromJSON.js
@@ -1,40 +1,47 @@
 var data = {
-    "Fish": {
-      "trout": {},
-      "salmon": {}
+  Fish: {
+    trout: {},
+    salmon: {}
+  },
+
+  Tree: {
+    Huge: {
+      sequoia: {},
+      oak: {}
     },
-  
-    "Tree": {
-      "Huge": {
-        "sequoia": {},
-        "oak": {}
-      },
-      "Flowering": {
-        "redbud": {},
-        "magnolia": {
-        }
-      }
+    Flowering: {
+      redbud: {},
+      magnolia: {}
     }
-  };
-  
-  
-  
-  const createDOMFromJSON = data => {
-    if(!data) return [];
-    
-    return Object.entries(data).reduce((acc, curr) => {
-      const [key, value] = curr;
-     
-      acc.push(`<li>${key}${Object.keys(value).length ? '<ul>'+ createDOMFromJSON(value) + '</ul>' : createDOMFromJSON(value)}</li>`);
-      return acc;                                
-    }, []).join('');
   }
-  
-  const createTree = (container, data) => {
-    container.innerHTML = `<ul>${createDOMFromJSON(data)}</ul>`;
-    
-  };
-  
-  const container = document.getElementById('container');
-  createTree(container, data);
-  
+};
+/**
+ *
+ * @param {object} data
+ * given a JSON structure, create dom nodes representing a tree
+ */
+const createDOMFromJSON = data => {
+  if (!data) return [];
+
+  return Object.entries(data)
+    .reduce((acc, curr) => {
+      const [key, value] = curr;
+      //if the object has properties, then wrap all the properties within a <ul></ul>
+      acc.push(
+        `<li>${key}${
+          Object.keys(value).length
+            ? '<ul>' + createDOMFromJSON(value) + '</ul>'
+            : createDOMFromJSON(value)
+        }</li>`
+      );
+      return acc;
+    }, [])
+    .join('');
+};
+
+const createTree = (container, data) => {
+  container.innerHTML = `<ul>${createDOMFromJSON(data)}</ul>`;
+};
+
+const container = document.getElementById('container');
+createTree(container, data);
